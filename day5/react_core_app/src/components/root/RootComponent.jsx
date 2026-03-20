f38859496d190d166440e0a057b4194395c90973
Dev TEdge <developer@technizeredge.com>
2026-03-20 10:41:36 +0530

    controlled and uncontrolled

--- day5/react_core_app/src/components/root/RootComponent.jsx
+++ day5/react_core_app/src/components/root/RootComponent.jsx
@@ -1,9 +1,12 @@
-import EventComponent from "../1_react_events/EventComponent";
+// import EventComponent from "../1_react_events/EventComponent";
+
+import ControlledVsUncontrolledComponent from "../2_controlled_vs_uncontrolled/ControlledVsUncontrolledComponent";
 
 const RootComponent = () => {
     return (
         <div className="container mt-4">
-            <EventComponent />
+            {/* <EventComponent /> */}
+            <ControlledVsUncontrolledComponent />
         </div>
     );
 }
